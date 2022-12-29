f3a9ad4b29069433eb15e91972533e41dd859026
betomelo92 <rag.betin@gmail.com>
2022-12-29 10:51:28 -0300

    Trocando reactDom do react 17 para o react 18

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './styles.css';
 
 import { Home } from './templates/Home';
 
-const reactdom = ReactDOM.createRoot(document.getElementById("root"));
-reactdom.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
